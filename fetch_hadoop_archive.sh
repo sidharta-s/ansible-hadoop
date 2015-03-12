5de26765ca6e543212151400578929d407a94ac5
Sidharta Seethana <sidharta.apache@gmail.com>
2015-03-12 14:31:29 -0700

    adding support for LinuxContainerExecutor

--- fetch_hadoop_archive.sh
+++ fetch_hadoop_archive.sh
@@ -1,1 +1,1 @@
-curl http://mirror.reverse.net/pub/apache/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz -o roles/common/files/hadoop-2.6.0.tar.gz
+curl http://mirror.reverse.net/pub/apache/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz -o roles/common/files/hadoop.tar.gz
